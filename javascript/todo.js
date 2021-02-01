89fcb0cf65ddb033b66cac21285268de12032f0d
S.Krithiik <krithiik1812@gmail.com>
2021-02-01 21:03:29 +0530

    initial commit

---
+++ javascript/todo.js
@@ -0,0 +1,23 @@
+$("ul").on("click","li",function(){
+    $(this).toggleClass("done");
+});
+
+$("ul").on("click","span",function(event){
+    event.stopPropagation();
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which===13){
+        var todo=$(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='fa fa-trash'></i></span> "+todo+"</li>");
+    }
+});
+
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+});
+
